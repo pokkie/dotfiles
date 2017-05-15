c1ffa6027673715ada2bd928b3e1224d71a9d672
dagnachewa <dagnachewa@gmail.com>
2017-05-15 01:23:50 -0400

    dwm added

---
+++ dwm/README.md
@@ -0,0 +1,16 @@
+# my dwm fork
+
+## patches
+- better border patch
+- circle tag symbols
+- flextile
+- middle statusbar
+- movestack
+- no background for title
+- pertag
+- save floats
+- statusallmons
+- swallow
+- systray
+- uselessgap
+- viewontag
---
+++ dwm/config.h
@@ -0,0 +1,132 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+
+static const char *fonts[] = {
+	"Ionicons:antialias=true:size=12",
+	"Terminus:antialias=true:size=10"
+};
+static const char dmenufont[] = "Meslo LG M for Powerline-8";
+
+static const char black[]                = "#282828";
+static const char gray[]                 = "#a89984";
+static const char white[]                = "#fbf1c7";
+static const char *colors[SchemeLast][3] = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { gray, black, black },
+	[SchemeSel] =  { white, black, white },
+};
+
+static const unsigned int borderpx       = 1;        /* border pixel of windows */
+static const unsigned int snap           = 16;       /* snap pixel */
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
+static const int showsystray             = 1;        /* 0 means no systray */
+static const int showbar                 = 1;        /* 0 means no bar */
+static const int topbar                  = 1;        /* 0 means bottom bar */
+static const Bool viewontag              = True;
+static const unsigned int gappx          = 5;
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	{ "Galculator",          NULL, NULL, 0, 1, 0, 0, -1 },
+	{ "Pavucontrol",         NULL, NULL, 0, 1, 0, 0, -1 },
+	{ "Gksu",                NULL, NULL, 0, 1, 0, 0, -1 },
+	{ "Mate-terminal",       NULL, NULL, 0, 0, 1, 0, -1 }
+};
+
+/* layout(s) */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int  layoutaxis[] = {
+	1,    /* layout axis: 1 = x, 2 = y; negative values mirror the layout */
+	2,    /* master axis: 1 = x (left to right), 2 = y (top to bottom), 3 = z (monocle) */
+	2,    /* stack  axis: 1 = x (left to right), 2 = y (top to bottom), 3 = z (monocle) */
+};
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */ 
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle }
+};
+
+/* key definitions */
+#define AltMask Mod1Mask
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", "#002b36", "-nf", "#568e75", "-sb", "#002b36", "-sf", "#268bd2", "-h", "22", NULL }; 
+static const char *termcmd[]  = { "termite", NULL };
+static const char *browsercmd[]  = { "google-chrome-stable", NULL }; 
+static const char *editorcmd[]  = { "subl3", NULL }; 
+
+#include <X11/XF86keysym.h>
+
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } }, 
+	{ MODKEY,                       XK_e,      spawn,          {.v = editorcmd } }, 
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_r,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_Left,   cycle,          {.i = -1 } },
+	{ MODKEY,                       XK_Right,  cycle,          {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Left,   tagcycle,       {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Right,  tagcycle,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Tab,    rotatelayoutaxis, {.i = 0} },    /* 0 = layout axis */
+	{ MODKEY|ControlMask,           XK_Tab,    rotatelayoutaxis, {.i = 1} },    /* 1 = master axis */
+	{ MODKEY|ControlMask|ShiftMask, XK_Tab,    rotatelayoutaxis, {.i = 2} },    /* 2 = stack axis */
+	{ MODKEY|ShiftMask,             XK_Return, mirrorlayout,     {0} },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+};
+
+static Button buttons[] = {
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
