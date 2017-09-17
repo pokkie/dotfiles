37e7cbc72fab3c23ff7e1668078bc72e2accd23a
dagnachewa <dagnachewa@gmail.com>
2017-09-17 16:58:13 -0400

    xst st fork update

--- xst/src/config.h
+++ xst/src/config.h
@@ -2,11 +2,11 @@
 
 #define histsize 2000
 
-static unsigned int cols = 80;
+static unsigned int cols = 63;
 static unsigned int rows = 24;
 
 /* ref: http://freedesktop.org/software/fontconfig/fontconfig-user.html */
-static char *font = "Iosevka Term:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=14:antialias=true:autohint=true";
 
 /* exec precedence: -e arg, utmp option, SHELL env var, /etc/passwd shell */
 static char *shell = "\0";
